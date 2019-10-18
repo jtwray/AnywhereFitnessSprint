0c476a13dc5de1fdfe32aab1cc068c20f7aabb43
Carl Padilla <carlpadilla@protonmail.com>
2019-10-17 21:10:51 -0400

    added react-router-dom axios and created axiosWithAuth file

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 
 function App() {
   return (
-    <div className='App'>
-      <Login />
-    </div>
+    <Router>
+      <div className='App'>
+        <Route exact path='/' component={Login} />
+      </div>
+    </Router>
   );
 }
 
