489fe9d9ee3484c7a1314eb1125217646bbf87ab
jtwray <tuckerwray84@gmail.com>
2019-10-22 18:24:12 -0400

    updates the baseurl axioswithAuth

--- anywhere-fitness/src/utils/axiosWithAuth.js
+++ anywhere-fitness/src/utils/axiosWithAuth.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
   return axios.create({
-    baseURL: `https://api-bookr.herokuapp.com/api/`,
+    baseURL: `https://lambda-fitness-anywhere.herokuapp.com/api/`,
+    headers: {
+      Authorization: token
+    }
+  });
+};
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
     headers: {
       Authorization: token
     }
