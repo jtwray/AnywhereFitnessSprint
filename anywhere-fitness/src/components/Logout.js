31d55f68a54d403954fa28404c0f73cd57b20706
jtwray <tuckerwray84@gmail.com>
2019-10-22 18:32:29 -0400

    creates an early navigationbar with two functioning links *dashboard & *logout /nvisible after auth on the dashboard. /nLogout button: *clears localStorage and *redirects to login view

---
+++ anywhere-fitness/src/components/Logout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Redirect} from 'react-router-dom'
+
+
+const Logout=() => {
+
+    localStorage.clear()
+    return <Redirect to='/login' />;
+
+}
+export default Logout
