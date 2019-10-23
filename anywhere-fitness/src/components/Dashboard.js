5bf6cd391a2e5cdf445d36568b600fb0b88b17a6
Carl Padilla <carlpadilla@protonmail.com>
2019-10-22 22:54:28 -0400

    working on dashboard styling

--- anywhere-fitness/src/components/Dashboard.js
+++ anywhere-fitness/src/components/Dashboard.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import Navigation from './Navigation'
- const Dashboard = (props)=>{
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SaveIcon from '@material-ui/icons/Save';
+import EditIcon from '@material-ui/icons/Edit';
 
-    return(
-        <>
-        <Navigation/>
-        <h1>DASHBOARD</h1>
-        <Link to="/classList" >ClassList</Link>
-        </>
-    )
-}
+const styles = {
+  // these buttons will be aligned to right side of abbBar
+  toolbarButtons: {
+    marginLeft: 'auto',
+    marginRight: -12
+  },
+  menuButton: {
+    marginRight: 20,
+    marginLeft: -12
+  }
+};
 
-export default Dashboard    
+const Dashboard = withStyles(styles)(({ classes }) => (
+  <AppBar position='static'>
+    <Toolbar>
+      <IconButton
+        className={classes.menuButton}
+        color='inherit'
+        aria-label='Menu'
+      >
+        <MenuIcon />
+      </IconButton>
+      <Typography variant='title' color='inherit'>
+        My Title
+      </Typography>
+      <span className={classes.toolbarButtons}>
+        <IconButton color='inherit' aria-label='Edit'>
+          <EditIcon />
+        </IconButton>
+        <IconButton color='inherit' aria-label='Save'>
+          <SaveIcon />
+        </IconButton>
+        <IconButton color='inherit' aria-label='More Options'>
+          <MoreVertIcon />
+        </IconButton>
+      </span>
+    </Toolbar>
+  </AppBar>
+));
+
+export default Dashboard;
